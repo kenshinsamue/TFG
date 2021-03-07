818f88c1b4b483b333ea5fc23ab92aba19fa1d51
k1k4ss0 <k1k4ss0@localhost.localdomain>
2021-03-07 16:59:25 -0500

    pruebas pyshark

--- BT/Sniffer.py
+++ BT/Sniffer.py
@@ -1,6 +1,7 @@
 from BT.Adaptador.Adaptador import *
 from scapy.layers.bluetooth import *
 from scapy.all import *
+import pyshark
 
 class Sniffer(object):
 
@@ -8,10 +9,33 @@ class Sniffer(object):
     self.AdaptadorLocal = Adaptador(adaptador)
     self.AdaptadorLocal.SetAdaptadorPropiedad("Discoverable","True")
   
+  def get_packet_layers(self,packet):
+    counter = 0
+    while True:
+        layer = packet.getlayer(counter)
+        if layer is None:
+            break
+
+        yield layer
+        counter += 1
+
   def ReceptorPaquete(self,pkt):
-    pkt.show()
+    # pkt.show2()
+    if pkt.getlayer(3) is not None: 
+      pkt.show()
+    # print ("Se ha encontrado un paquete del host: {}".format(pkt))
+    # for layer in self.get_packet_layers(pkt):
+    #   print("{}".format(pkt.getlayer(3)))
+    #   pkt.show()
+    #   print(layer.name)
+
 
   def start (self):
-    bt = BluetoothHCISocket(0)
-    bt.sniff(prn=self.ReceptorPaquete)
-  
+    # bt = BluetoothHCISocket(0)
+    # bt.sniff(prn=self.ReceptorPaquete, count=0 )
+    capture = pyshark.LiveCapture(interface='bluetooth0')
+    while True:
+      capture.sniff(packet_count=1)
+      if capture[0].layers[2] is not None:
+        print(capture[0].layers[2].name) 
+
